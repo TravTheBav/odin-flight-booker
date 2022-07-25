16a5108111893ec756283131afff6b0a71a29523
Travis <tzito93@gmail.com>
2022-07-25 11:58:00 -0500

    Airport and flight models with associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Airport.delete_all
+Flight.delete_all
+
+Airport.create!([
+  {
+    code: "JFK"
+  },
+  {
+    code: "SAT"
+  },
+  {
+    code: "LAX"
+  }
+])
+
+Flight.create!([
+  {
+    departure_airport_id: Airport.first.id,
+    arrival_airport_id: Airport.last.id,
+    start_datetime: Time.now,
+    flight_duration: 6.0
+  },
+  {
+    departure_airport_id: Airport.last.id,
+    arrival_airport_id: Airport.first.id,
+    start_datetime: Time.now.next_day,
+    flight_duration: 6.0
+  }
+])
+
+p "#{Airport.count} airports created"
+p "#{Flight.count} flights created"
