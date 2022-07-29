ac25d67243d3db447043002aeb831b2f677a920f
Travis <tzito93@gmail.com>
2022-07-29 11:00:54 -0500

    Search form returns all flights that match the entered criteria

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,9 +1,27 @@
 class FlightsController < ApplicationController
   def index
-    @airport_options = Airport.all.map { |a| [a.code, a.id] }
+    @airport_options = Airport.select(:code, :id).map { |a| [a.code, a.id] }
     @passenger_options = [['1', 1], ['2', 2], ['3', 3], ['4', 4]]
     @flight_day_options = Flight.format_dates(:day)
     @flight_month_options = Flight.format_dates(:month)
     @flight_year_options = Flight.format_dates(:year)
+
+    if params[:commit].present? # if a search was entered
+      @flights = Flight.find_flights(
+        departure_airport_id: params[:departure_airport_id],
+        arrival_airport_id: params[:arrival_airport_id],
+        flight_date: Time.parse(format_date_from_params)
+      )
+    else # when index page was reached without a search
+      @flights = []
+    end
+  end
+
+  private
+
+  def format_date_from_params
+    params[:flight_date][2] + '-' +
+    params[:flight_date][1].rjust(2, '0') + '-' +
+    params[:flight_date][0].rjust(2, '0')
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,24 +2,26 @@ class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: 'Airport'
   belongs_to :arrival_airport, class_name: 'Airport'
 
-  def self.all_distinct
-    Flight.select(:start_datetime).distinct
-  end
-
   def self.format_dates(type)
-    flights = Flight.all_distinct
+    flights = Flight.select(:start_datetime).distinct
 
     case type
     when :day
-      flights.map { |f| [f.start_datetime.strftime('%d'), f.start_datetime.strftime('%d').to_i]}
+      flights = flights.map { |f| [f.start_datetime.strftime('%d'), f.start_datetime.strftime('%d').to_i]}
     when :month
       flights = flights.map { |f| [f.start_datetime.strftime('%m'), f.start_datetime.strftime('%m').to_i]}
-      flights.uniq
     when :year
-      flights = flights.map { |f| [f.start_datetime.strftime('%y'), f.start_datetime.strftime('%y').to_i]}
-      flights.uniq
+      flights = flights.map { |f| [f.start_datetime.strftime('%Y'), f.start_datetime.strftime('%Y').to_i]}
     else
       raise 'must pass in day, month, or year as a symbol'
     end
+
+    flights.uniq
+  end
+
+  def self.find_flights(departure_airport_id:, arrival_airport_id:, flight_date:)
+    Flight.where(departure_airport_id: departure_airport_id.to_i)
+          .where(arrival_airport_id: arrival_airport_id.to_i)
+          .where(start_datetime: (flight_date..(flight_date + (60 * 60 * 24) - 1))) # checks between midnight and 11:59 PM
   end
 end
