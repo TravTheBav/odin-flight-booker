491b35b6399315ecfaf03edc3a516c7521167799
Travis <tzito93@gmail.com>
2022-08-04 11:45:21 -0500

    Successful bookings redirect to bookings#show action

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,8 +6,19 @@ class BookingsController < ApplicationController
     total_passengers.times { @booking.passengers.build }
   end
 
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
   def create
-    @booking = Booking.create!(booking_params)
+    @booking = Booking.create(booking_params)
+    if @booking.valid?
+      flash[:notice] = 'Your trip has been booked'
+      redirect_to booking_path(@booking)
+    else
+      flash[:errors] = @booking.errors.full_messages
+      redirect_to new_booking_path
+    end
   end
 
   private
